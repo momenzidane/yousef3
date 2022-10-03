0fbed4d7185879c28122ac9a59e2ba58269ff285
momenzidane <105398269+momenzidane@users.noreply.github.com>
2022-10-03 21:34:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+let menu = document.getElementById("munu_id");
+let mune_data = document.getElementById("mune_data");
+
+menu.addEventListener("click", function () {
+    mune_data.style.display ="block"
+});
+
+
+
+
+
